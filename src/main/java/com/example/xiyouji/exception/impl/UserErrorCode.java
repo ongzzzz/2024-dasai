8aaca053149a74161c4d8e9d47ca47c5ad57f8c8
jaeseonNamgung <sunnamgung8@naver.com>
2024-03-27 17:11:46 +0800

    회원 중복검사 추가 코드 구현 및 테스트

--- src/main/java/com/example/xiyouji/exception/impl/UserErrorCode.java
+++ src/main/java/com/example/xiyouji/exception/impl/UserErrorCode.java
@@ -11,7 +11,8 @@ public enum UserErrorCode implements ErrorCode {
     OK(HttpStatus.OK, "success"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않은 회원입니다."),
     USER_RANKING_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않은 회원 랭킹입니다."),
-    ALREADY_WROTE_COMMENT(HttpStatus.BAD_REQUEST, "한 계정당 1회 작성 가능합니다.");
+    ALREADY_WROTE_COMMENT(HttpStatus.BAD_REQUEST, "한 계정당 1회 작성 가능합니다."),
+    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/example/xiyouji/login/repository/MemberRepository.java
+++ src/main/java/com/example/xiyouji/login/repository/MemberRepository.java
@@ -4,6 +4,7 @@ package com.example.xiyouji.login.repository;
 import com.example.xiyouji.login.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> , MemberQueryDslRepository{
 }
